e34c48884bafe43b417fceb1e6f4c49d51e5b82f
PietroFP812 <pietro-fiorelli@hotmail.com>
2024-04-02 20:20:31 -0300

    Menu

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main()
+void main()
 {
-    printf("projeto:\n");
+    printf("Trabalho ORDENACÃO E TÉCNICAS DE ARMAZENAMENTO\n");
+    printf("Alunos: Eduardo de Quadra, Gustavo Mello, Pietro Fiorelli e Wallan Gabriel\n");
+    printf("Turma: 0103N\n");
+    printf("###############################################################################\n");
+    printf("###############################################################################\n");
+    printf("# 1 - Inserir Elemento no Final                                               #\n");
+    printf("# 2 -                                                                         #\n");
+    printf("# 3 - Remover Elemento do Final                                               #\n");
+    printf("# 4 -                                                                         #\n");
+    printf("# 5 - Consultar Elemento Pela Posição                                         #\n");
+    printf("# 6 -                                                                         #\n");
+    printf("# 7 - Exibir Tamanho da Lista                                                 #\n");
+    printf("# 8 - Sair                                                                    #\n");
+    printf("###############################################################################\n");
+    printf("###############################################################################\n");
     return 0;
 }
