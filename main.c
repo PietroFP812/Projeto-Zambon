82d9c0c0dc10ee4f5bce7525f1be93854eb1a4b1
PietroFP812 <pietro-fiorelli@hotmail.com>
2024-04-03 08:17:43 -0300

    upd Menu, locale

--- main.c
+++ main.c
@@ -4,19 +4,20 @@
 
 void main()
 {
-    printf("Trabalho ORDENACÃO E TÉCNICAS DE ARMAZENAMENTO\n");
-    printf("Alunos: Eduardo de Quadra, Gustavo Mello, Pietro Fiorelli e Wallan Gabriel\n");
-    printf("Turma: 0103N\n");
+    setlocale(LC_ALL,"");
+    printf("#####           Trabalho ORDENACÃO E TÉCNICAS DE ARMAZENAMENTO           ######\n");
+    printf("  Alunos: Eduardo de Quadra, Gustavo Mello, Pietro Fiorelli e Wallan Gabriel   \n");
+    printf("#############################  Turma: 0103N  ##################################\n");
     printf("###############################################################################\n");
     printf("###############################################################################\n");
     printf("# 1 - Inserir Elemento no Final                                               #\n");
-    printf("# 2 -                                                                         #\n");
-    printf("# 3 - Remover Elemento do Final                                               #\n");
-    printf("# 4 -                                                                         #\n");
-    printf("# 5 - Consultar Elemento Pela Posição                                         #\n");
-    printf("# 6 -                                                                         #\n");
-    printf("# 7 - Exibir Tamanho da Lista                                                 #\n");
-    printf("# 8 - Sair                                                                    #\n");
+    printf("#####                                                                         #\n");
+    printf("# 2 - Remover Elemento do Final                                               #\n");
+    printf("#####                                                                         #\n");
+    printf("# 3 - Consultar Elemento Pela Posição                                         #\n");
+    printf("#####                                                                         #\n");
+    printf("# 4 - Exibir Tamanho da Lista                                                 #\n");
+    printf("# 5 - Sair                                                                    #\n");
     printf("###############################################################################\n");
     printf("###############################################################################\n");
     return 0;
